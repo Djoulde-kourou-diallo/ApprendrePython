45486c643cca281bc20307d37af397bcc79af6ee
DjouldeKourouDiallo <mdjoulde512@gmaial.com>
2025-03-15 00:55:34 +0000

    EXO1

--- 100DaysOnPython/day_#1/main.py
+++ 100DaysOnPython/day_#1/main.py
@@ -1,3 +1,19 @@
-print("Bienvenue dans notre programme 1OODAYSONPYTHON")
-print("Bienvenue dans notre programme 1OODAYSONPYTHON")
-print("Bienvenue dans notre programme 1OODAYSONPYTHON")
+# print("Bienvenue dans notre programme 1OODAYSONPYTHON")
+# print("Bienvenue dans notre programme 1OODAYSONPYTHON")
+# print("Bienvenue dans notre programme 1OODAYSONPYTHON")
+# DECLARATION DES VARRIABLES
+# a = 2 + 5j
+# b = 4 + 6j
+# print(a + b)
+# CONVERSION DES VARRIABLES
+# a = 2.5
+# b = int(a)
+# print(a)
+# print(b)
+# L AFFICHAGE 
+# nom = "Mamadou"
+# age = 40
+# print("Nom :{1}, Age:{0}".format(nom, age))
+# LES ENTRERS AU CLAVIER
+nom = input("Donner votre nom")
+print(f"Bonjour, {nom}")
