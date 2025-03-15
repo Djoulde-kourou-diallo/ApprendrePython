7a21e360d1ae02249335e9ef1d7cd88167146804
DjouldeKourouDiallo <mdjoulde512@gmaial.com>
2025-03-15 00:52:22 +0000

    C'est l'exo 1

---
+++ 100DaysOnPython/day_#1/exo1.py
@@ -0,0 +1,42 @@
+# exo1
+nom = ("Djoulde")
+print(nom)
+#EXO2
+# age = 45
+# print(f"J'ai: {age} ans")
+#EXO3
+# ville = ("Mamou")
+# print(f"J'habite à: {ville}")
+# #EXO4
+# a = 5
+# b = 3
+# print(a + b)
+#EXO5
+# x = 10
+# y = 2.5
+# z = "Python"
+# print(x, y, z)
+#EXO6
+# prenom = input("Donnez votre prénom : ")
+# print(f"Bonjour {prenom}")
+#EXO7
+# age = input("Donnez votre âge :")
+# print(f"Tu as : {age} ans")
+#EXO8
+# a = int(input("Donnez un nombre :"))
+# b = int(input("Donnez un nombre :"))
+# som = (a + b)
+# print(som)
+#EXO9
+# nom = input("Donnez votre nom :")
+# age = input("Donnez votre âge :")
+# print(f"Tu t'appelles : {nom} et tu as : {age} ans")
+#EXO510
+# x = 10
+# y = 2.5
+# z = "Python"
+# print(type(x), type(y), type(z))
+#EXO11
+# a = int(input("Entrez un nombre :"))
+# nb = a * 2
+# print(nb)
