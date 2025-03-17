5372a0a61b715870fcfd752313067c680b8d7137
DjouldeKourouDiallo <mdjoulde512@gmaial.com>
2025-03-17 02:34:05 +0000

    C'est l'exo2

---
+++ 100DaysOnPython/day_#1/niveau1.py
@@ -0,0 +1,102 @@
+# exo1
+# nom = ("Djoulde")
+# print(nom)
+
+#EXO2
+age = 45
+print(f"J'ai: {age} ans")
+
+#EXO3
+# ville = ("Mamou")
+# print(f"J'habite à: {ville}")
+
+# #EXO4
+# a = 5
+# b = 3
+# print(a + b)
+
+#EXO5
+# x = 10
+# y = 2.5
+# z = "Python"
+# print(x, y, z)
+
+#EXO6
+# prenom = input("Donnez votre prénom : ")
+# print(f"Bonjour {prenom}")
+
+#EXO7
+# age = input("Donnez votre âge :")
+# print(f"Tu as : {age} ans")
+
+#EXO8
+# a = int(input("Donnez un nombre :"))
+# b = int(input("Donnez un nombre :"))
+# som = (a + b)
+# print(som)
+
+#EXO9
+# nom = input("Donnez votre nom :")
+# age = input("Donnez votre âge :")
+# print(f"Tu t'appelles : {nom} et tu as : {age} ans")
+
+#EXO510
+# x = 10
+# y = 2.5
+# z = "Python"
+# print(type(x), type(y), type(z))
+
+#EXO11
+# a = int(input("Entrez un nombre :"))
+# nb = a * 2
+# print(nb)
+
+#EXO12
+# nb = int(input("Donnez un nombre :"))
+# carre = nb * nb
+# print(carre)
+
+#EXO13
+# char = "Bonjour le monde"
+# print(char[0])
+
+#EXO14
+# a = int(input("Donnez un nombre :"))
+# b = int(input("Donnez un nombre :"))
+# res = (a * b)
+# print(res)
+
+#EXO15
+# clsus = float(input("Donnez la temperature en celsus :"))
+# fahrenheit = clsus * 9/5 + 32
+# print(f"La temperature en fahrenheit : {fahrenheit} °F")
+
+ #EXO16
+# prix_ht = float(input("Donnez le prix de l'article :"))
+# tva = float(input("Quel est le taux de pourcentage "))
+# prix_ttc = prix_ht * (1 + tva / 100)
+# # print(f"Le prix ttc de l'article est : {prix_ttc} gnf")
+
+#EXO17
+# nb = int(input("Donnez un nombre :"))
+# if nb % 2 == 0:
+#     print("Nombre pair")
+# else: print("Nombre impair")
+
+#EXO18
+# mot = "Mamadou"
+# res = len(mot)
+# print(f"Le mot contient {res} letres")
+
+#EXO19
+# prenom = input("Quel est votre prénom ?")
+# age = int(input("Donnez votre âge actuel :"))
+# age_futur = age + 10
+# print(f"Monsieur {prenom} dans 10 ans vous aurez {age_futur} ans")
+
+# #EXO20
+# a = 5
+# b = 9
+# print(f"Avant l'echange : a {a} et b {b}")
+# a, b = b, a
+# print(f"Après echange : a {a} et b {b}")
---
+++ 100DaysOnPython/day_#1/niveau2.py
@@ -0,0 +1,35 @@
+#EXO21
+# prenom = input("Donnez votre prénom :")
+# print("Monsieur", prenom.upper())
+# print("Monsieur", prenom.lower())
+#EXO22
+# nombre1 = int(input("Donnez le premier nombre :"))
+# nombre2 = int(input("Donnez le deuxième nombre :"))
+# qt = nombre1 // nombre2
+# res = nombre1 % nombre2
+# print(f"Le quotient est : {qt}")
+# print(f"Le reste est : {res}")
+#EXO23
+# nombre1 = float(input("Donnez le premier nombre :"))
+# nombre2 = float(input("Donnez le deuxième nombre :"))
+# nombre3 = float(input("Donnez le deuxième nombre :"))
+# moyenne = (nombre1 + nombre2 + nombre3) / 3
+# print(F"La moyenne des trois nombres est :{moyenne}")
+#EXO 24
+# longueur_m = int(input("Donnez la longueur en mètre "))
+# lon_km = longueur_m / 1000
+# print(f"{longueur_m} mètres equivalent à {lon_km} kilomètres")
+#EXO25
+# phrase = input("Donnez une phrase :")
+# for i in range(5):
+#     print(phrase)
+#EXO26
+# annee = int(input("Donnez une annee :"))
+# if annee % 4 == 0 and (annee % 100 != 0 or annee % 400 == 0):
+#     print(f"L'annee {annee} est bissesxtile")
+# else: print(f"L'annee {annee} n'est pas bissextile")
+#EXO27
+# prix = float(input("Quel est le prix du produit ? "))
+# reduction = float(input("Quel est le pourcentage de reduction ?"))
+# prix_reduit = prix - (prix * reduction /100)
+# print(f"Le prix final après reduction est : {prix_reduit} gnf")
